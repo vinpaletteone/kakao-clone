53a069b9a8db70276a9b177bb5767322beb28067
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-04 20:19:32 +0900

    reset-scss 설치

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Main } from './js/routes'
+import { Main } from './js/routes';
+import 'sass-reset'
 
 function App() {
   return (
-    <BrowserRouter className="App">
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter className="App">
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
