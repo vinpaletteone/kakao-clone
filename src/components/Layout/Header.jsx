af50acf0ee6049e4cb53a813289da4a27ad84912
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-05 16:24:57 +0900

    다크모드 toggle 기능 추가

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,50 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BodyClassName from 'react-body-classname'
 import '../../styles/_header.scss'
 
-export default function header() {
+export default function Header() {
+    const [isDark, setDark] = useState(false);
+
+    const themeChange = () => {
+        console.log(isDark)
+      isDark ? setDark(false) : setDark(true);
+    }; 
+    
     return (
-        <header>
-            <div className="wrap-header">
-                <h1>
-                    <a href="/">
-                        <span>KaKao</span>
+        <BodyClassName className={isDark ? 'dark' : 'light'}>
+            <header>
+                <div className="wrap-header">
+                    <h1>
+                        <a href="/">
+                            <span>KaKao</span>
+                            logo
+                        </a>
                         logo
-                    </a>
-                    logo
-                </h1>
-                <nav>
-                    <h2>메인메뉴</h2>
-                    <ul className="nav-list">
-                        <li>
-                            <a href="/">카카오</a>
-                        </li>
-                        <li>
-                            <a href="/">뉴스</a>
-                        </li>
-                        <li>
-                            <a href="/">기술과 서비스</a>
-                        </li>
-                        <li>
-                            <a href="/">약속과 책임</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="area-util">
-                    <strong>유틸 기능</strong>
-                    <button>
-                        <span>검색창 열기</span>
-                        검색
-                    </button>
-                    <button>
-                        <span>영어로 보기</span>
-                        언어
-                    </button>
-                    <button>
-                        <span>다크모드로 변경하기</span>
-                        테마
-                    </button>
+                    </h1>
+                    <nav>
+                        <h2>메인메뉴</h2>
+                        <ul className="nav-list">
+                            <li>
+                                <a href="/">카카오</a>
+                            </li>
+                            <li>
+                                <a href="/">뉴스</a>
+                            </li>
+                            <li>
+                                <a href="/">기술과 서비스</a>
+                            </li>
+                            <li>
+                                <a href="/">약속과 책임</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="area-util">
+                        <strong>유틸 기능</strong>
+                        <button>
+                            <span>검색창 열기</span>
+                            검색
+                        </button>
+                        <button>
+                            <span>영어로 보기</span>
+                            언어
+                        </button>
+                        <button onClick={themeChange}>
+                            <span>다크모드로 변경하기</span>
+                            테마
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </BodyClassName>
     )
 }
