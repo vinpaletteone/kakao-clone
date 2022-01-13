761d6220c26f168287d707207abf311956f75d1d
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-14 00:15:45 +0900

    카드형 배너 레이아웃 추가

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,7 +3,127 @@ import React from 'react'
 export default function ItemCard() {
     return (
         <section className='main-itemCard'>
-            item
+            <div className="section-main">
+                <div className="wrap-item">
+                    <div className="inner-item">
+                        <div className="item-card">
+                            <span className="wrap-cont">
+                                <span role="text" className="info-cate">
+                                    <span className="txt-cate">보도자료</span>
+                                    <span className="dot-new">new</span>
+                                </span>
+                                <a href="/" className='link-item'>
+                                    <strong className="tit-card">제목</strong>
+                                    <span className="wrap-thumb">
+                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
+                                    </span>
+                                </a>
+                                <span role="text" className="info-card">
+                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
+                                </span>
+                            </span>
+                            <button className="btn-share">
+                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="wrap-item">
+                    <div className="inner-item">
+                        <div className="item-card">
+                            <span className="wrap-cont">
+                                <span role="text" className="info-cate">
+                                    <span className="txt-cate">보도자료</span>
+                                    <span className="dot-new">new</span>
+                                </span>
+                                <a href="/" className='link-item'>
+                                    <strong className="tit-card">제목</strong>
+                                    <span className="wrap-thumb">
+                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
+                                    </span>
+                                </a>
+                                <span role="text" className="info-card">
+                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
+                                </span>
+                            </span>
+                            <button className="btn-share">
+                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="inner-item">
+                        <div className="item-card">
+                            <span className="wrap-cont">
+                                <span role="text" className="info-cate">
+                                    <span className="txt-cate">보도자료</span>
+                                    <span className="dot-new">new</span>
+                                </span>
+                                <a href="/" className='link-item'>
+                                    <strong className="tit-card">제목</strong>
+                                    <span className="wrap-thumb">
+                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
+                                    </span>
+                                </a>
+                                <span role="text" className="info-card">
+                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
+                                </span>
+                            </span>
+                            <button className="btn-share">
+                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="inner-item">
+                        <div className="item-card">
+                            <span className="wrap-cont">
+                                <span role="text" className="info-cate">
+                                    <span className="txt-cate">보도자료</span>
+                                    <span className="dot-new">new</span>
+                                </span>
+                                <a href="/" className='link-item'>
+                                    <strong className="tit-card">제목</strong>
+                                    <span className="wrap-thumb">
+                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
+                                    </span>
+                                </a>
+                                <span role="text" className="info-card">
+                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
+                                </span>
+                            </span>
+                            <button className="btn-share">
+                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="inner-item">
+                        <div className="item-card">
+                            <span className="wrap-cont">
+                                <span role="text" className="info-cate">
+                                    <span className="txt-cate">보도자료</span>
+                                    <span className="dot-new">new</span>
+                                </span>
+                                <a href="/" className='link-item'>
+                                    <strong className="tit-card">제목</strong>
+                                    <span className="wrap-thumb">
+                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
+                                    </span>
+                                </a>
+                                <span role="text" className="info-card">
+                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
+                                </span>
+                            </span>
+                            <button className="btn-share">
+                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
