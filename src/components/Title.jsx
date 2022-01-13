c82c7e10b4f05b6d9d95ddf2e024c0f75f02337c
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-13 19:30:30 +0900

    날짜 구하기, 날짜 url 삽입

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default function Title() {
+    const now = new Date();
+    const month = now.getMonth()+1;
+    const date = now.getDate();
+    const day = now.getDay();
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+ 
+    const dateImage = {
+        background: `url("https://www.kakaocorp.com/page/calendar/light/ico_date${date}.gif") no-repeat 0 94px / 72px 72px`
+    }
     return (
-        <div>
-            title
-        </div>
+        <section className="main-title">
+            <h2 className="screen-out">홈 본문</h2>
+            <div className="wrap-fit">
+                <h3 className='tit-main' style={dateImage}>
+                    <em className="emph-tit">오늘의 카카오</em>
+                    <span>{month}월 {date}일 {week[day]}요일 소식입니다</span>
+                </h3>
+            </div>
+        </section>
     )
 }
