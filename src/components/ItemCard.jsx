56de216b65b584d21b7ca168a964cbb07f251c0d
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-14 23:24:42 +0900

    card 배너 레이아웃 수정

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -54,25 +54,25 @@ export default function ItemCard() {
                         </div>
                     </div>
                     <div className="inner-item">
-                        <div className="item-card">
+                        <div className="item-card card-half item-stock">
                             <span className="wrap-cont">
                                 <span role="text" className="info-cate">
                                     <span className="txt-cate">보도자료</span>
                                     <span className="dot-new">new</span>
                                 </span>
                                 <a href="/" className='link-item'>
-                                    <strong className="tit-card">제목</strong>
+                                    <strong className="tit-card">
+                                        <span className="num-tit">3</span>
+                                        <span className="num-info down"><span className="screen-out">전일대비</span><span className="info-arr">하락</span>2800</span>
+                                    </strong>
                                     <span className="wrap-thumb">
                                         <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
                                     </span>
                                 </a>
-                                <span role="text" className="info-card">
-                                    <span class="txt_keyword">#CAC</span><span class="txt_keyword">#규정안</span><span class="txt_keyword">#임원주식매도</span><span class="txt_keyword">#카카오계열사</span>
-                                </span>
+                                <span role="text" className="info-card">2022.01.14 22:08PM</span>
                             </span>
                             <button className="btn-share">
                                 <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
-                                {/* <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico-share"><g data-v-f9455140="" fill="none" fill-rule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg> */}
                             </button>
                         </div>
                     </div>
@@ -100,7 +100,7 @@ export default function ItemCard() {
                         </div>
                     </div>
                     <div className="inner-item">
-                        <div className="item-card">
+                        <div className="item-card card-half">
                             <span className="wrap-cont">
                                 <span role="text" className="info-cate">
                                     <span className="txt-cate">보도자료</span>
