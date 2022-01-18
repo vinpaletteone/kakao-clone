070cff1e6f70740e7f051a0cf56534b16c696bea
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-18 19:30:37 +0900

    컴포넌트 경로 추가

--- src/js/component.js
+++ src/js/component.js
@@ -3,3 +3,5 @@ export { default as Footer } from '../components/Layout/Footer';
 export { default as Title } from '../components/Title'
 export { default as ItemCard } from '../components/ItemCard'
 export { default as ClickBtn } from '../components/ClickBtn'
+export { default as Culture } from '../components/Culture'
+export { default as ItemCard2 } from '../components/ItemCard2'
