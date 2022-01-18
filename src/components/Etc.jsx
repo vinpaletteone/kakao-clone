56563820ac57ff3e78b011e33c0ad0379953663d
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-19 01:51:57 +0900

    Etc 추가

---
+++ src/components/Etc.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Etc() {
+    return (
+        <section className='main-etc'>
+            <div className="item-etc">
+                <a href="" className='link-etc link-service'>
+                    <strong className="tit-item">사람과 기술로 일상을 돕는 카카오 서비스</strong>
+                    <span className="txt-item">
+                        서비스 바로가기
+                        <svg data-v-d93d847a="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-arr"><g data-v-d93d847a="" fill="none" fillRule="evenodd"><g data-v-d93d847a="" stroke="#FFF" strokeWidth="1.5" className="stroke"><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><path data-v-d93d847a="" d="M.625 0L9.846 0 9.846 9.221M9.846 0L.654 9.192" transform="translate(-567 -1210) translate(36 1044) translate(134 54) translate(254 98) translate(20 9) translate(121 4) rotate(45 2.879 10.121)"></path></g></g></g></g></g></g></g></g></svg>
+                    </span>
+                </a>
+            </div>
+            <div className="item-etc">
+                <a href="" className='link-etc link-recruit'>
+                    <strong className="tit-item">세상만사에 관심이 많다면, 당신은 이미 카카오 크루</strong>
+                    <span className="txt-item">
+                        인재영입 바로가기
+                        <svg data-v-d93d847a="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-arr"><g data-v-d93d847a="" fill="none" fillRule="evenodd"><g data-v-d93d847a="" stroke="#FFF" strokeWidth="1.5" className="stroke"><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><path data-v-d93d847a="" d="M.625 0L9.846 0 9.846 9.221M9.846 0L.654 9.192" transform="translate(-567 -1210) translate(36 1044) translate(134 54) translate(254 98) translate(20 9) translate(121 4) rotate(45 2.879 10.121)"></path></g></g></g></g></g></g></g></g></svg>
+                    </span>
+                </a>
+            </div>
+        </section>
+    )
+}
