6b23ec36f1def042a7fdfccf451a7e2841777ff9
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-18 19:23:20 +0900

    item 반복 렌더링

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,57 +1,29 @@
 import React from 'react';
 import dummy from '../db/mainItem.json'
+import Item from './Item';
 
 export default function ItemCard({icon}) {
     console.log(dummy);
+    console.log(dummy.main7DaysBeforeList);
+    const beforeList = dummy.main7DaysBeforeList;
+    
+    console.log(beforeList);
+    // console.log(beforeList[0]);
     return (
         <section className='main-itemCard'>
             <div className="section-main">
                 <div className="wrap-item">
                     <div className="inner-item">
-                        <div className="item-card">
-                            <span className="wrap-cont">
-                                <span role="text" className="info-cate">
-                                    <span className="txt-cate">보도자료</span>
-                                    <span className="dot-new">new</span>
-                                </span>
-                                <a href="/" className='link-item'>
-                                    <strong className="tit-card">제목</strong>
-                                    <span className="wrap-thumb">
-                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
-                                    </span>
-                                </a>
-                                <span role="text" className="info-card">
-                                    <span className="txt_keyword">#CAC</span><span className="txt_keyword">#규정안</span><span className="txt_keyword">#임원주식매도</span><span className="txt_keyword">#카카오계열사</span>
-                                </span>
-                            </span>
-                            <button className="btn-share">
-                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="ico-share"><g data-v-f9455140="" fill="none" fillRule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" className="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
-                            </button>
-                        </div>
+                        <Item item={beforeList[0]} itemNew={'Y'}/>
                     </div>
                 </div>
                 <div className="wrap-item">
                     <div className="inner-item">
-                        <div className="item-card">
-                            <span className="wrap-cont">
-                                <span role="text" className="info-cate">
-                                    <span className="txt-cate">보도자료</span>
-                                    <span className="dot-new">new</span>
-                                </span>
-                                <a href="/" className='link-item'>
-                                    <strong className="tit-card">제목</strong>
-                                    <span className="wrap-thumb">
-                                        <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/51de11ea017e00001.png?type=thumb&opt=C630x472" alt="" className="thumb-img" />
-                                    </span>
-                                </a>
-                                <span role="text" className="info-card">
-                                    <span className="txt_keyword">#CAC</span><span className="txt_keyword">#규정안</span><span className="txt_keyword">#임원주식매도</span><span className="txt_keyword">#카카오계열사</span>
-                                </span>
-                            </span>
-                            <button className="btn-share">
-                                <svg data-v-f9455140="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="ico-share"><g data-v-f9455140="" fill="none" fillRule="evenodd"><path data-v-f9455140="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-f9455140="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" className="fill"><circle data-v-f9455140="" cx="2" cy="2" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="10" r="2"></circle> <circle data-v-f9455140="" cx="2" cy="18" r="2"></circle></g></g></svg>
-                            </button>
-                        </div>
+                        {
+                            beforeList.map((item, index) => {
+                              return index!==0 && <Item item={item} />
+                            })
+                        }
                         <div className="item-card card-half item-stock">
                             <a href="https://www.kakaocorp.com/ir/stockInformation" className="wrap-cont">
                                 <div role="text">
