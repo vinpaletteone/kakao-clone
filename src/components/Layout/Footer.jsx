448630a4b6ed997bb76b9ba73e51e485704818ec
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-19 01:53:57 +0900

    Footer 컴포넌트 추가,  Nav List 데이터 가져와 FooterNav 컴포넌트 분리

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import footerList from '../../db/footerList.json'
+import '../../styles/_footer.scss'
+import FooterNav from './FooterNav';
 
 export default function Footer() {
     return (
-        <div>
-            footer
-        </div>
+        <footer>
+            <div className="inner-footer">
+                <section className='section-service clear'>
+                    <h2 className="screen-out">하단 메뉴</h2>
+                    {
+                        footerList.map((item, index)=> <FooterNav key={index} item={item}/>)
+                    }
+                </section>
+            </div>
+        </footer>
     )
 }
