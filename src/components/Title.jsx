1f0826aea1eb563201ac685208ddf8bb7b6291ef
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-19 23:03:02 +0900

    props 로 변경

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 
-export default function Title({isDark}) {
-    const now = new Date();
-    const month = now.getMonth()+1;
-    const date = now.getDate();
-    const day = now.getDay();
-    const week = ['일', '월', '화', '수', '목', '금', '토'];
- 
-    const dateImage = {
-        background: `url("https://www.kakaocorp.com/page/calendar/${!isDark ? 'light' : 'dark'}/ico_date${date}.gif") no-repeat 0 94px / 72px 72px`
-    }
+export default function Title({...props}) {
     return (
         <section className="main-title">
             <h2 className="screen-out">홈 본문</h2>
             <div className="wrap-fit">
-                <h3 className='tit-main' style={dateImage}>
-                    <em className="emph-tit">오늘의 카카오</em>
-                    <span>{month}월 {date}일 {week[day]}요일 소식입니다</span>
+                <h3 className='tit-main' style={props.dateImage}>
+                    <em className="emph-tit">{props.headerTitle}</em>
+                    <span>{props.month}월 {props.date}일 {props.day}요일 소식입니다</span>
                 </h3>
             </div>
         </section>
