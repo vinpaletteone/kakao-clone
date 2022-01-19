3638d13558c294bc8a4c90336067e9a5300da0ea
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-19 23:05:19 +0900

    top btn 컴포넌트 분리

---
+++ src/components/Layout/LinkTop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LinkTop() {
+    return (
+        <a href="" className='link-top'>
+            <span role='text'>
+            <span className="screen-out">맨 위로 이동</span>
+            <svg data-v-2e94cb1c="" data-v-1c5f090a="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="ico-top"><g data-v-2e94cb1c="" fill="none" fillRule="evenodd"><g data-v-2e94cb1c="" transform="translate(-1318 -4124) translate(1318 4124)"><circle data-v-2e94cb1c="" cx="25" cy="25" r="25"></circle> <g data-v-2e94cb1c="" strokeWidth="1.5" className="stroke"><path data-v-2e94cb1c="" d="M8.467 16.933L0 8.467 8.467 0M.085 8.467L19.918 8.534" transform="rotate(90 9.5 24)"></path></g></g></g></svg>
+            </span>
+        </a>
+    )
+}
