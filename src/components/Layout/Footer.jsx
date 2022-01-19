d88971cd808722cdb28bc4ed5576c4c0c085a2a5
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-19 23:03:51 +0900

    relation, 관련 사이트 추가

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import footerList from '../../db/footerList.json'
+import dummy from '../../db/footer.json'
 import '../../styles/_footer.scss'
-import FooterNav from './FooterNav';
+import FooteRelation from './Footer/FooterRelation';
+import FooterNav from './Footer/FooterNav';
 
 export default function Footer() {
     return (
@@ -10,9 +11,46 @@ export default function Footer() {
                 <section className='section-service clear'>
                     <h2 className="screen-out">하단 메뉴</h2>
                     {
-                        footerList.map((item, index)=> <FooterNav key={index} item={item}/>)
+                        dummy.footerList.map((item, index)=> <FooterNav key={index} item={item}/>)
                     }
                 </section>
+                <section className='section-relation clear'>
+                    <h2 className="screen-out">서비스 이용정보</h2>
+                    <div className="group-info clear">
+                        {
+                            dummy.FooterRelation.map((item, index)=> <FooteRelation key={index} item={item}/>)
+                        }
+                    </div>
+                    <div className="wrap-relation">
+                        <strong className="tit-relation">
+                            <a href="" role="button" className='clear'>
+                                관련사이트
+                                <svg data-v-75150fa2="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-plus"><path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398)"></path> <path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398) rotate(90 7 7)"></path></svg>
+                            </a>
+                        </strong>
+                        <ul className='list-subinfo'>
+                        {
+                            dummy.RelationSite && dummy.RelationSite.map((i, index)=>{
+                                return (
+                                    <li key={index}>
+                                        <a href={i.link} target='_blank'>
+                                            <span role='text'>
+                                                {i.name}
+                                                <span className="screen-out">사이트 바로가기</span>
+                                                <svg data-v-c1d21be8="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>
+                                            </span>
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                        </ul>
+                    </div>
+                </section>
+               <small className="txt-copyright">
+                   © <a href="https://www.kakaocorp.com/page/">Kakao Corp.</a>
+                   All rights reserved.
+               </small>
             </div>
         </footer>
     )
