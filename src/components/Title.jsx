b91f25b1d43f37664b326b6bd5848d38eb868e80
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-20 23:46:59 +0900

    모바일 텍스트 조건부 렌더링

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive';
 
 export default function Title({...props}) {
+    const isMobile = useMediaQuery({
+        query : "(max-width:766px)"
+    });
+
     return (
         <section className="main-title">
             <h2 className="screen-out">홈 본문</h2>
             <div className="wrap-fit">
                 <h3 className='tit-main' style={props.dateImage}>
                     <em className="emph-tit">{props.headerTitle}</em>
-                    <span>{props.month}월 {props.date}일 {props.day}요일 소식입니다</span>
+                    <span>{props.month}월 {props.date}일 {props.day}요일 소식{!isMobile && '입니다'}</span>
                 </h3>
             </div>
         </section>
