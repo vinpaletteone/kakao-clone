cefb98401a2ceec1f0e4bda4803d57b191a78ef0
“vinpaletteone”  <binsuzy95@gmail.com>
2022-01-20 23:34:28 +0900

    commit

--- src/components/Culture.jsx
+++ src/components/Culture.jsx
@@ -9,7 +9,7 @@ export default function Culture() {
                 <svg data-v-d93d847a="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-arr"><g data-v-d93d847a="" fill="none" fillRule="evenodd"><g data-v-d93d847a="" stroke="#FFF" strokeWidth="1.5" className="stroke"><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><g data-v-d93d847a=""><path data-v-d93d847a="" d="M.625 0L9.846 0 9.846 9.221M9.846 0L.654 9.192" transform="translate(-567 -1210) translate(36 1044) translate(134 54) translate(254 98) translate(20 9) translate(121 4) rotate(45 2.879 10.121)"></path></g></g></g></g></g></g></g></g></svg>
             </a>
             <img src="https://www.kakaocorp.com/page/bg_home_culture_210325.png" alt="" className="thumb-culture thumb-culture-l"></img>
-            <img src="/page/bg_home_culture_s_210325.png" alt="" className="thumb-culture thumb-culture-s"></img>
+            <img src="https://www.kakaocorp.com/page/bg_home_culture_s_210325.png" alt="" className="thumb-culture thumb-culture-s"></img>
         </section>
     )
 }
