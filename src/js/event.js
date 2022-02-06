decedcb6590497689a65089bec51e397781510b0
“vinpaletteone”  <binsuzy95@gmail.com>
2022-02-06 13:24:41 +0900

    header scroll event 추가

--- src/js/event.js
+++ src/js/event.js
@@ -1,14 +1,40 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const header = document.querySelector('header');
+    const headerFeature = document.querySelector('.header-feature');
 
-    console.log(header);
+    let beforePosition = 0;
+    headerFeature.style.display = 'none';
+
     window.addEventListener('scroll', (e)=>{
-        let scrollLocation = document.documentElement.scrollTop; 
-
-        if(scrollLocation > 0){
+        let afterPosition = document.documentElement.scrollTop; 
+        
+        if(afterPosition > 0){
             header.classList.add('active');
         }else{
             header.classList.remove('active');
+        }
+
+        if(afterPosition>=160){
+            headerFeature.style.display = 'block';
+
+            if(beforePosition < afterPosition){
+                //scroll down
+                setTimeout(()=>{
+                    header.classList.add('hide');
+                    headerFeature.classList.add('active')
+                },0)
+            }else{
+                //scroll up
+                header.classList.remove('hide');
+                headerFeature.classList.remove('active');
+            }
+            beforePosition = afterPosition;
+
+        }else{
+            header.classList.remove('hide');
+            headerFeature.style.display = 'none';
+            headerFeature.classList.remove('active');
+
         }
 
     });
