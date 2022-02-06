5f9c37c476f8b89b0c659cd1ad399ce8e0c15b9c
“vinpaletteone”  <binsuzy95@gmail.com>
2022-02-07 03:37:00 +0900

    footer sub-info click event 추가

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -25,7 +25,7 @@ export default function Footer() {
                         <strong className="tit-relation">
                             <a href="" role="button" className='clear'>
                                 관련사이트
-                                <svg data-v-75150fa2="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-plus"><path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398)"></path> <path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398) rotate(90 7 7)"></path></svg>
+                                <svg data-v-75150fa2=""  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="ico-plus"><path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398)"></path> <path data-v-75150fa2="" d="M6.222 0H7.7780000000000005V14H6.222z" transform="translate(-1414 -643) translate(80 245) translate(1334 398) rotate(90 7 7)"></path></svg>
                             </a>
                         </strong>
                         <ul className='list-subinfo'>
@@ -37,7 +37,7 @@ export default function Footer() {
                                             <span role='text'>
                                                 {i.name}
                                                 <span className="screen-out">사이트 바로가기</span>
-                                                <svg data-v-c1d21be8="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>
+                                                <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>
                                             </span>
                                         </a>
                                     </li>
--- src/components/Layout/Footer/FooterNav.jsx
+++ src/components/Layout/Footer/FooterNav.jsx
@@ -9,10 +9,10 @@ export default function FooterNav({item}) {
                 <a href={item.link}>
                     <span role="text">
                         {item.name}
-                        {item.name==='고객센터' || flag && <span className="screen-out" data-v-f9d51b02="">사이트 바로가기</span>}
+                        {item.name==='고객센터' || flag && <span className="screen-out" >사이트 바로가기</span>}
                         {
-                            !flag ? <svg data-v-d180c69a="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-arrow"><g data-v-d180c69a="" fill="none" fillRule="evenodd"><path data-v-d180c69a="" d="M0 0H9V9H0z" transform="translate(-891 -972) translate(670 145) translate(24 817) translate(142 4) translate(55 6)"></path> <path data-v-d180c69a="" d="M8 3L4.5 6 1 3" transform="translate(-891 -972) translate(670 145) translate(24 817) translate(142 4) translate(55 6)" className="stroke"></path></g></svg> : 
-                            <svg data-v-c1d21be8="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>
+                            !flag ? <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-arrow"><g  fill="none" fillRule="evenodd"><path  d="M0 0H9V9H0z" transform="translate(-891 -972) translate(670 145) translate(24 817) translate(142 4) translate(55 6)"></path> <path  d="M8 3L4.5 6 1 3" transform="translate(-891 -972) translate(670 145) translate(24 817) translate(142 4) translate(55 6)" className="stroke"></path></g></svg> : 
+                            <svg data-v-c1d21be8=""  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>
                         }
                     </span>
                 </a>
@@ -24,7 +24,7 @@ export default function FooterNav({item}) {
                             <li role="listItem" key={index}>
                                 <a href={i.link}>
                                     <span role="text">{i.name}</span>
-                                    {item.name==='고객센터' && <svg data-v-c1d21be8="" data-v-f9d51b02="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>}
+                                    {item.name==='고객센터' && <svg data-v-c1d21be8=""  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" className="ico-outlink"><g data-v-c1d21be8="" fill="none" fillRule="evenodd"><path data-v-c1d21be8="" d="M1.795 1.074L7.942 1.074 7.942 7.221M7.942 1.074L1.378 7.638" transform="translate(-935 -867) translate(836 848) translate(14 14) translate(85 5)"></path></g></svg>}
                                 </a>
                             </li>
                         )
